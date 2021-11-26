24806edd6c47167ea81496319e589f7453e22db7
Derek Robles Cuevas <derek.robles@upr.edu>
2021-11-26 19:53:15 -0400

    Fix about route

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('', (req,res)=>{
 });
 
 app.get('/about', (req, res)=>{
-    res.render('About',{
+    res.render('about',{
         title: 'About Me',
         name: 'Derek Robles'
     });
